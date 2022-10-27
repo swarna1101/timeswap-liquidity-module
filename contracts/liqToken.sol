a1a08111d8dc7792a55ef89a40924295ccbda1d7
swarna1101 <113277972+swarna1101@users.noreply.github.com>
2022-10-27 23:47:17 +0530

    fix:typo

--- contracts/liqToken.sol
+++ contracts/liqToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
-contract MyNFT is ERC1155, Ownable, ERC1155Burnable {
+contract liqToken is ERC1155, Ownable, ERC1155Burnable {
     constructor() ERC1155("") {}
 
     function setURI(string memory _newURI) public onlyOwner {

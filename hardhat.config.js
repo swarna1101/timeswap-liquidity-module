113468797417ae009bda0598ebe9157ef6aafefe
Swarnabha Sinha <swarnabhasinha.201mn059@nitk.edu.in>
2022-10-27 21:29:11 +0530

    update: hardhat configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,7 @@
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
-
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.17',
-  networks: {
-    rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`${RINKEBY_PRIVATE_KEY}`],
-    },
-  },
+  solidity: "0.8.17",
 };
